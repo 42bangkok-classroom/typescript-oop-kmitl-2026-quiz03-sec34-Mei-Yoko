7b5b7f5d0cdb7f6e4b45440b2a60c7997f51832d
Mei-Yoko <keypongphob@gmail.com>
2026-02-20 18:36:30 +0700

    last2

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,3 +1,38 @@
-export class OnlineCourse {}
+export class OnlineCourse {
+    courseName: string;
+    maxStudents: number;
+    private enrolledStudents: number = 0;
+    private isOpen: boolean = true;
 
-public
+    constructor(courseName: string, maxStudents: number) {
+        this.courseName = courseName;
+        this.maxStudents = maxStudents;
+    }
+
+    private canEnroll(): boolean{
+        return this.isOpen && this.enrolledStudents < this.maxStudents;
+    }
+    enroll(): boolean {
+        if (this.canEnroll()) {
+            this.enrolledStudents++;
+            return true;
+        }
+        return false;
+    }
+
+    closeCourse(): void {
+        this.isOpen = false;
+    }
+
+    getAvailableSeats(): number {
+        return this.maxStudents - this.enrolledStudents;
+    }
+
+    getCourseStatus(): string {
+        return this.isOpen ? "Open" : "Closed";
+    }
+}
+    
+
+
+
