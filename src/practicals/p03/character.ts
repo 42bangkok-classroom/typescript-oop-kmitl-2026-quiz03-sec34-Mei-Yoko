439837f6d02cc8372d98b3fe16113634ff084cce
Mei-Yoko <keypongphob@gmail.com>
2026-02-20 18:08:03 +0700

    adadadada

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,21 @@
-export class Character {}
+export class Character {
+    name: string;
+    health: number = 100;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getHealth(): number {
+        return this.health;
+    }
+
+    receiveDamage(damage: number): void {
+        this.health -= damage;
+    }
+
+}
--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -1,5 +1,20 @@
 import { Character } from "./character";
 
 export class Warrior extends Character {
+  weapon: string;
+
+  constructor(name: string, weapon: string, health: number){
+    super(name);
+    this.weapon = weapon;
+    this.health = health;
+  }
+
+  getWeapon(): string{
+    return this.weapon;
+  }
+
+  override  receiveDamage(damage: number): void {
+    this.health -= damage * 0.9;
+  }
   // TODO: implement class properties, constructor with super(...), and methods
 }
