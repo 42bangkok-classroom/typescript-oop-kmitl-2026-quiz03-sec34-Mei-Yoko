821fa2a793f1d3776a6f92d7f2f051a35441baf9
Mei-Yoko <keypongphob@gmail.com>
2026-02-20 17:54:50 +0700

    2

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,36 @@
-export class User {}
+export class User {
+    public firstname: string ="";
+    public lastname: string ="";
+    private age: number= 0;
+    static BIRTH_YEAR: number = 2000;
+
+    constructor(firstname: string = "", lastname: string ="", age: number = 0){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    setFirstname(firstname: string): void{
+        this.firstname = firstname;
+    }
+
+    setLastname(lastname: string): void{
+        this.lastname = lastname;
+    }
+
+    setAge(age: number): void{
+        this.age = age;
+    }
+    
+    getAge(): number{
+        return this.age;
+    }
+
+    getFullname(): string{
+        return `$(this.firstname) $(this.lastname)`;
+    }
+
+
+}
+
+
